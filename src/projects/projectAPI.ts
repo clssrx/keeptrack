470c5acdbdbdcd102736d6dc8cfb874e67471767
clssrx <48386524+clssra@users.noreply.github.com>
2023-09-01 00:54:15 +0200

    setup backend and get wip

---
+++ src/projects/projectAPI.ts
@@ -0,0 +1,31 @@
+
+const baseUrl = 'http://localhost:4000';
+const url = `${baseUrl}/projects`
+
+const delay = (ms: number) => {
+  return function (x: any): Promise<any> {
+    return new Promise(resolve => setTimeout(() => resolve(x), ms))
+  }
+}
+
+const checkStatus = (response: any) => {
+  if(response.ok)
+    return response
+  else{
+    const httpErrorInfo = {
+      status: response.status,
+      statusText: response.statusText,
+      url: response.url,
+    }
+
+    console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+  }
+}
+
+const ProjectAPI = {
+  get(page=1, limit=20){
+    return fetch(`${url}?_page=${page}&_limit=${limit}`)
+            .then(delay(600))
+            .then(checkStatus)
+  }
+}
