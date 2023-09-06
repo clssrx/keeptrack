5f5b606b61bd72c52acfe48d4881613092d5212f
clssrx <48386524+clssra@users.noreply.github.com>
2023-09-06 23:16:12 +0200

    add form validation

--- src/components/ErrorCard.tsx
+++ src/components/ErrorCard.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 interface ErrorCardType {
   errorMessage: string | undefined;
+  dimension?: 'large',
 }
 
-const ErrorCard = ({ errorMessage }: ErrorCardType) => {
+const ErrorCard = ({ errorMessage, dimension }: ErrorCardType) => {
   return (
     <div className="row">
-      <div className="card large error">
+      <div className={"card error" + dimension}>
         <section>
           <p>
             <span className="icon-alert inverse "></span>
